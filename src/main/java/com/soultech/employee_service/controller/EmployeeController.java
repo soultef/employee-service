2d9961f02295d408682870019ca4fc0a3f2cdc42
soultef <soul_tefera@yahoo.com>
2025-05-17 09:07:27 -0500

    feat: add paginated GET /employees endpoint with default sorting by firstName

--- src/main/java/com/soultech/employee_service/controller/EmployeeController.java
+++ src/main/java/com/soultech/employee_service/controller/EmployeeController.java
@@ -1,4 +1,32 @@
 package com.soultech.employee_service.controller;
 
+import com.soultech.employee_service.DTO.EmployeeDTO;
+import com.soultech.employee_service.service.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+
+@RestController
+@RequestMapping("/api/v1")
 public class EmployeeController {
+
+    private final EmployeeService employeeService;
+
+    @Autowired
+    public EmployeeController(EmployeeService employeeService) {
+        this.employeeService = employeeService;
+    }
+
+    @GetMapping("/employees")
+    public ResponseEntity<Page<EmployeeDTO>> getAllEmployees(
+            @PageableDefault(size = 100, sort = "firstName", direction = Sort.Direction.ASC) Pageable pageable) {
+
+        Page<EmployeeDTO> employees = employeeService.getAllEmployees(pageable);
+        return ResponseEntity.ok(employees);
+    }
 }
